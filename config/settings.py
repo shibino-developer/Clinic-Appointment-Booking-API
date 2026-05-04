07ef0c1e2855461a5d619d3464796e7bf25939a0
shibino-developer <shibino.developer@gmail.com>
2026-05-04 18:14:40 +0530

    changed settings

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,6 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
